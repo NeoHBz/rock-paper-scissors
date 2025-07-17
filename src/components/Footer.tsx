64fada1b8574cbedadb2a0ac908994821936f51f
NeoHBz <neohbz@gmail.com>
2025-07-17 11:30:05 +0530

    removed: footer credits

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ export const Footer: React.FC = () => {
     <footer className="flex-grow sticky bottom-0">
       <div className="flex justify-end items-center w-full h-12 pt-4 pr-4 gap-2 opacity-50 text-sm">
         <a
-          href="https://neohbz.com"
+          // href="https://neohbz.com"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-white"
+          className="text-gray-700 dark:text-white hover:text-white transition-colors duration-300"
         >
-          Made with ❤️ by Saurav
+          Made with ❤️ by Shalini
         </a>
       </div>
     </footer>
