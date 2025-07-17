c64ed4241b13e975ef87669969363c30c78ab0f2
NeoHBz <neohbz@gmail.com>
2025-07-17 13:27:56 +0530

    removed: navbar render

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Footer } from "./components/Footer";
 import { HomeScreen } from "./components/Homescreen";
-import { Navbar } from "./components/Navbar";
 
 export const App = () => {
   return (
     <div className="flex flex-col min-h-screen w-screen">
-      <Navbar />
       <HomeScreen />
       <Footer />
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import fontHelper from "../utils/fontHelper";
 export const Navbar: React.FC = () => {
   return (
     <div className="min-w-full min-h-12">
-      <nav className="navbar flex bg-red-700/50 shadow-lg w-full h-20 fixed top-0 z-50">
+      <nav className="navbar flex bg-red-500/80 shadow-lg w-full h-20 fixed top-0 z-50">
         <div className="flex justify-center items-center w-full h-full">
           <span
             className={`${fontHelper("font-pixeboy")} text-4xl md:text-5xl lg:text-6xl`}
