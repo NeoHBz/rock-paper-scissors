5aa4f6d5d52c836a8abb7cf25c0ad259d82e01ee
NeoHBz <neohbz@gmail.com>
2025-07-22 22:08:07 +0530

    fix: reverted back footer credits

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ export const Footer: React.FC = () => {
     <footer className="grow sticky bottom-0">
       <div className="flex justify-end items-center w-full h-12 pt-4 pr-4 gap-2 text-sm">
         <a
-          // href="https://neohbz.com"
+          href="https://neohbz.com"
           target="_blank"
           rel="noopener noreferrer"
           className="text-white hover:text-black transition-colors duration-300 font-pixeboy"
         >
-          Made with ❤️ by Shalini
+          Made with ❤️ by Saurav
         </a>
       </div>
     </footer>
